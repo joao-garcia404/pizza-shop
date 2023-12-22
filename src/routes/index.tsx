234f75de8ee0c1253a014860b99b37a90dbaf752
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-12-22 19:26:02 -0300

    Refactor: Folders organization

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Dashboard } from "@/pages/app/dashboard";
-import { SignIn } from "@/pages/auth/sign-in";
+import { Dashboard } from "@/view/pages/app/dashboard";
+import { SignIn } from "@/view/pages/auth/sign-in";
 
 export const router = createBrowserRouter([
   {
