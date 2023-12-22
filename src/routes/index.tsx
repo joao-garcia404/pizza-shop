8a4a60be200f47d9c1e953aa98eda96b5fa178e0
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-12-22 19:31:08 -0300

    Feat: Base signin and dashboard layouts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { AppLayout } from "@/view/layouts/app";
+import { AuthLayout } from "@/view/layouts/auth";
+
 import { Dashboard } from "@/view/pages/app/dashboard";
 import { SignIn } from "@/view/pages/auth/sign-in";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: "sign-in",
-    element: <SignIn />,
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
