1effe667362aa8c0a7b64067158d0f65668a238f
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-12-23 15:18:39 -0300

    Feat: App layout header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+
 import { Toaster } from "sonner";
+
+import { ThemeProvider } from "./app/contexts/theme-context";
 
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
