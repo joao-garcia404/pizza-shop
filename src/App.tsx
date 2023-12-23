1946a85434be635f21f68c048c2107741830c58b
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-12-23 13:56:01 -0300

    Feat: Sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 import { router } from "./routes";
 
@@ -7,6 +8,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors closeButton />
       <RouterProvider router={router} />
     </HelmetProvider>
   );
--- src/view/pages/auth/sign-in.tsx
+++ src/view/pages/auth/sign-in.tsx
@@ -1,12 +1,69 @@
+import { useForm } from "react-hook-form";
+
 import { Helmet } from "react-helmet-async";
+import { z } from "zod";
+
+import { Button } from "@/view/components/ui/button";
+import { Input } from "@/view/components/ui/input";
+import { Label } from "@/view/components/ui/label";
+import { toast } from "sonner";
+
+const signInFormSchema = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInFormSchema>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
+
+      toast.success("Enviamos um link de autenticação para seu e-mail.");
+    } catch {
+      toast.error("Credenciais inválidas.");
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
 
-      <div>
-        <h1>Sign In</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form
+            onSubmit={handleSubmit(handleSignIn)}
+            className="flex flex-col gap-4"
+          >
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input type="email" {...register("email")} />
+            </div>
+
+            <Button type="submit" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
+          </form>
+        </div>
       </div>
     </>
   );
