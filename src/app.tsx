edb6ff7f942db27d41bb609897a7bfa4304c4276
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2024-01-20 00:03:01 -0300

    Feat: Setup react-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "./app/contexts/theme-context";
+
+import { queryClient } from "./app/lib/react-query";
 
 import { router } from "./routes";
 
@@ -13,7 +16,10 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
