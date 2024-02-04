402a2d8cb75d85a6a80e35dc5a8dfde870f243ab
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2024-02-04 13:25:25 -0300

    Feat: Automatic logout

--- src/view/layouts/app.tsx
+++ src/view/layouts/app.tsx
@@ -1,8 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
+import { Outlet, useNavigate } from "react-router-dom";
+
+import { isAxiosError } from "axios";
+import { toast } from "sonner";
+
+import { httpClient } from "@/app/lib/axios";
 
 import { Header } from "../components/header";
 
 export function AppLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interceptorId = httpClient.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error?.response?.data?.code;
+
+          if (status === 401 && code === "UNAUTHORIZED") {
+            toast.error("Sessão expirada, faça login novamente.");
+            navigate("/sign-in", { replace: true });
+          }
+        }
+      },
+    );
+
+    return () => {
+      httpClient.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
